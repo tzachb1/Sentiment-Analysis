30ef597203675c3d31783633038a8b09ca82924b
Tzach Bar <tzachb@gmail.com>
2017-05-17 01:46:56 +0300

    additional changes

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from keras.layers import Flatten, Dense, Dropout, Embedding, LSTM, Bidirectional
 from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
 
-def base_model(params):
+def base(params):
     model = Sequential()
     model.add(Embedding(params['top_words'], params['embedding_vecor_length'], input_length=params['max_review_length']))
     model.add(Flatten())
@@ -11,7 +11,7 @@ def base_model(params):
     model.add(Dense(1, activation='sigmoid'))
     return model
 
-def conv_model(params):
+def conv(params):
     model = Sequential()
     model.add(Embedding(params['top_words'], params['embedding_vecor_length'], input_length=params['max_review_length']))
     model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
@@ -20,18 +20,31 @@ def conv_model(params):
     model.add(Dense(250, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     return model
+
+def conv2(params):
+    model = Sequential()
+    model.add(Embedding(params['top_words'], params['embedding_vecor_length'], input_length=params['max_review_length']))
+    model.add(Convolution1D(64, 3, border_mode='same'))
+    model.add(Convolution1D(32, 3, border_mode='same'))
+    model.add(Convolution1D(16, 3, border_mode='same'))
+    model.add(Flatten())
+    model.add(Dropout(0.2))
+    model.add(Dense(180,activation='sigmoid'))
+    model.add(Dropout(0.2))
+    model.add(Dense(1,activation='sigmoid'))
+    return model   
     
-def lstm_model(params):
+def lstm(params):
     model = Sequential()
     model.add(Embedding(params['top_words'], params['embedding_vecor_length'], input_length=params['max_review_length']))
     model.add(LSTM(100))
     model.add(Dense(1, activation='sigmoid'))
     return model
     
-def bi_lstm_model(params):
+def lstm2(params):
     model = Sequential()
     model.add(Embedding(params['top_words'], params['embedding_vecor_length'], input_length=params['max_review_length']))
-    model.add(Bidirectional(LSTM(64)))
-    model.add(Dropout(0.5))
+    model.add(LSTM(200))
+    model.add(Dropout(0.2))
     model.add(Dense(1, activation='sigmoid'))
     return model
